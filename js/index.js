9dee654056f885538136128d9a6393179d81b94a
Brian Boy <brian.boy@gmx.ch>
2022-02-10 18:35:40 +0100

    Media queries and code cleanup

--- js/index.js
+++ js/index.js
@@ -228,6 +228,37 @@ const acceptCookies = function () {
   containerCookies.classList.remove("show-cookies");
 };
 
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scroll({
+        top: 0,
+        behavior: "smooth",
+      });
+      console.log("home");
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // // Close mobile naviagtion
+    // if (link.classList.contains("main-nav-link"))
+    //   headerEl.classList.toggle("nav-open");
+  });
+});
+
 /////////////////////////////////////////////////////////////////////
 // HANDLER //
 /////////////////////////////////////////////////////////////////////
