ddc9bb755755f502e3e18ea0446af66b9c228fda
Brian Boy <brian.boy@gmx.ch>
2022-05-02 11:20:41 +0200

    PHP for header and footer

---
+++ blog.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta name="robots" content="index, follow, max-image-preview:large" />
+    <meta
+      name="description"
+      content="Ihre persönliche Webagentur für modernes Webdesign 🖥. Durchdacht, handgemacht und massgeschneidert."
+    />
+    <meta
+      name="keywords"
+      content="Website erstellen lassen, Homepage erstellen lassen, Webdesign Agentur, Webagentur, Webdesign, Webentwicklung, Homepage erstellen, neue Website erstellen, Schweiz"
+    />
+
+    <meta name="theme-color" content="#49475b" />
+
+    <meta property=“og:image:secure_url“
+    content=“https://derseitenschneider.ch/content/favicons/og-img.png“/>
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:title" content="Der Seitenschneider" />
+    <meta
+      property="og:description"
+      content="Ihre persönliche Webagentur für modernes Webdesign 🖥. Durchdacht, handgemacht und massgeschneidert."
+    />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://derseitenschneider.ch" />
+    <meta property="og:locale" content="de_CH" />
+    <meta
+      property="article:publisher"
+      content="https://www.facebook.com/derseitenschneider"
+    />
+
+    <link rel="cannonical" href="https://derseitenschneider.ch" />
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-VCH61NHG1Q"
+    ></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "G-VCH61NHG1Q");
+    </script>
+
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600&display=swap");
+    </style>
+
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap");
+    </style>
+
+    <link href="css/general.css" rel="stylesheet" />
+    <link href="css/index.css" rel="stylesheet" />
+    <link href="css/queries.css" rel="stylesheet" />
+
+    <link rel="icon" href="content/favicons/favicon-64.png" />
+    <link rel="apple-touch-icon" href="content/favicons/apple-touch-icon.png" />
+    <link rel="manifest" href="manifest.webmanifest" />
+
+    <script src="js/index.js" defer></script>
+    <script src="js/animation.js" defer></script>
+    <script src="js/modernizr-custom.js" defer></script>
+
+    <title>Der Seitenschneider</title>
+  </head>
+<body>
+  <?php
+  include('comp/header.php');
+  ?>
+
+
+
+
+<?php
+include('comp/footer.php');
+?>
+  
+</body>
+</html>
---
+++ comp/header.php
@@ -0,0 +1,255 @@
+<header class="header">
+
+      <a href="#portfolio" class="btn btn-skip-nav">
+        Navigation überspringen
+      </a>
+      <div class="container-header">
+        <a href="index.php" class="logo-header">
+          <svg
+            class="icon icon-logo"
+            width="607"
+            height="607"
+            viewBox="0 0 607 607"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_1_44)">
+              <rect
+                x="607"
+                y="607"
+                width="607"
+                height="607"
+                rx="303.5"
+                transform="rotate(-180 607 607)"
+                fill="#F8F7F7"
+              />
+              <circle
+                cx="303.5"
+                cy="303.5"
+                r="269.778"
+                transform="rotate(-180 303.5 303.5)"
+                fill="#799496"
+              />
+              <path
+                d="M280 446.933C280 469.282 261.882 487.4 239.533 487.4L0 487.4V366H239.533C261.882 366 280 384.117 280 406.467V446.933Z"
+                fill="#F8F7F7"
+              />
+              <path
+                d="M607 250.4L367.467 250.4C345.118 250.4 327 232.282 327 209.933V169.467C327 147.117 345.118 129 367.467 129L607 129V250.4Z"
+                fill="#F8F7F7"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1_44">
+                <rect
+                  x="607"
+                  y="607"
+                  width="607"
+                  height="607"
+                  rx="303.5"
+                  transform="rotate(-180 607 607)"
+                  fill="white"
+                />
+              </clipPath>
+            </defs>
+          </svg>
+
+          <p class="logo-title">der seitenschneider</p>
+        </a>
+        <ul class="main-nav">
+          <li class="nav-el">
+            <a
+              href="index.php"
+              class="nav-link nav-link--home"
+              >Home</a
+            >
+          </li>
+          <li class="nav-el">
+            <a href="index.php#portfolio" class="nav-link nav-link--portfolio"
+              >Portfolio</a
+            >
+          </li>
+          <li class="nav-el">
+            <a href="index.php#angebot" class="nav-link nav-link--angebot">Angebot</a>
+          </li>
+          <li class="nav-el">
+            <a href="index.php#schneider" class="nav-link nav-link--schneider"
+              >Der Schneider</a
+            >
+          </li>
+          <li class="nav-el">
+            <a href="blog.php" class="nav-link nav-link--schneider"
+              >Blog</a
+            >
+          </li>
+          <li class="nav-el">
+            <a href="index.php#contact" class="nav-link nav-link--contact">Kontakt</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="container-scroll-nav">
+      <div class="wrapper-scroll-nav">
+        <div class="logo-scroll-nav">
+          <a href="#" class="link-logo" tabindex="-1" aria-hidden="true">
+            <svg
+              class="icon icon-logo icon-logo-scrollnav"
+              width="607"
+              height="607"
+              viewBox="0 0 607 607"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clip-path="url(#clip0_1_44)">
+                <rect
+                  x="607"
+                  y="607"
+                  width="607"
+                  height="607"
+                  rx="303.5"
+                  transform="rotate(-180 607 607)"
+                  fill="#F8F7F7"
+                />
+                <circle
+                  cx="303.5"
+                  cy="303.5"
+                  r="269.778"
+                  transform="rotate(-180 303.5 303.5)"
+                  fill="#49475B"
+                />
+                <path
+                  d="M280 446.933C280 469.282 261.882 487.4 239.533 487.4L0 487.4V366H239.533C261.882 366 280 384.117 280 406.467V446.933Z"
+                  fill="#F8F7F7"
+                />
+                <path
+                  d="M607 250.4L367.467 250.4C345.118 250.4 327 232.282 327 209.933V169.467C327 147.117 345.118 129 367.467 129L607 129V250.4Z"
+                  fill="#F8F7F7"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_1_44">
+                  <rect
+                    x="607"
+                    y="607"
+                    width="607"
+                    height="607"
+                    rx="303.5"
+                    transform="rotate(-180 607 607)"
+                    fill="white"
+                  />
+                </clipPath>
+              </defs>
+            </svg>
+          </a>
+          <p class="title-scrollnav">der seitenschneider</p>
+        </div>
+
+        <ul class="scroll-nav">
+          <li class="el-scroll-nav">
+            <a
+              href="#"
+              class="link-scroll-nav"
+              onclick="openCloseScrollNav()"
+              tabindex="-1"
+              aria-hidden="true"
+              >Home</a
+            >
+          </li>
+
+          <li class="el-scroll-nav">
+            <a
+              href="#portfolio"
+              class="link-scroll-nav"
+              onclick="openCloseScrollNav()"
+              tabindex="-1"
+              aria-hidden="true"
+              >Portfolio</a
+            >
+          </li>
+
+          <li class="el-scroll-nav">
+            <a
+              href="#angebot"
+              class="link-scroll-nav"
+              onclick="openCloseScrollNav()"
+              tabindex="-1"
+              aria-hidden="true"
+              >Angebot</a
+            >
+          </li>
+
+          <li class="el-scroll-nav">
+            <a
+              href="#schneider"
+              class="link-scroll-nav"
+              onclick="openCloseScrollNav()"
+              tabindex="-1"
+              aria-hidden="true"
+              >Der Schneider</a
+            >
+          </li>
+
+          <li class="el-scroll-nav">
+            <a
+              href="blog.php"
+              class="link-scroll-nav"
+              onclick="openCloseScrollNav()"
+              tabindex="-1"
+              aria-hidden="true"
+              >Blog</a
+            >
+          </li>
+
+          <li class="el-scroll-nav">
+            <a
+              href="#contact"
+              class="link-scroll-nav"
+              onclick="openCloseScrollNav()"
+              tabindex="-1"
+              aria-hidden="true"
+              >Kontakt</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="container-btn-scroll-nav">
+      <button
+        class="btn btn-scroll-nav btn-scroll-nav--open"
+        aria-label="Navigation öffnen und schliessen"
+      >
+        <svg
+          class="icon icon-scrollnav icon-scrollnav--open"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
+        </svg>
+
+        <svg
+          class="icon icon-scrollnav icon-scrollnav--close"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <title>Close</title>
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="32"
+            d="M368 368L144 144M368 144L144 368"
+          />
+        </svg>
+      </button>
+    </div>
+    </header>
+  
